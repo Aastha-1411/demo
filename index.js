e11894b9091d42973260eef4b7b6a5abd8321a6d
Aastha Mishra <aasthamishra1103@gmail.com>
2025-04-17 21:45:36 +0530

    done

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const name = "Aastha"
+const name1 = "Aastha"
 
 const age = "21"
 
 function addi(a,b){
     return a+b
+}
+
+function myname(){
+    console.log("hii its "+name1);
+    
+}
+
+function isit18(){
+    return age>18
 }
